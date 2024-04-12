49914f3ed8555d0c6ba4be1279b211e3f385c3fe
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-04-12 22:12:56 +0530

    shadcn added

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -2,17 +2,17 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
-import { structure } from './structure'
-import { defaultDocumentNode } from './structure/defaultDocumentNode'
+import {structure} from './structure'
+import {defaultDocumentNode} from './structure/defaultDocumentNode'
 
 export default defineConfig({
   name: 'default',
   title: 'Day one with Sanity',
 
-  projectId: 'dwxaajvq',
-  dataset: 'production',
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID as string,
+  dataset: process.env.SANITY_STUDIO_DATASET as string,
 
-  plugins: [structureTool({structure,defaultDocumentNode}), visionTool()],
+  plugins: [structureTool({structure, defaultDocumentNode}), visionTool()],
 
   schema: {
     types: schemaTypes,
