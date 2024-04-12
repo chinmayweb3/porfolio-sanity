35740e756535702451fba37d99139e81303d02b8
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-04-12 23:12:47 +0530

    ssr sanity set and sanity added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 import { Button } from "@/components/ui/button";
+import { client } from "@/lib/sanityClient";
 import Image from "next/image";
 
-export default function Home() {
+export const revalidate = 10; // revalidate at most every hour
+
+export default async function Home() {
+  const alldata = await client.fetch(`*[_type=="venue"]`);
+
+  console.log("get add the data: ", alldata);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
+          {/* {alldata} */}
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
